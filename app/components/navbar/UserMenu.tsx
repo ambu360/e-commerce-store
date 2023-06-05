f693ab520b40e88333f91a292455e942bcdd74f7
ambu360 <ayman360@outlok.com>
2023-06-04 23:01:45 -0400

    updated navbar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,24 +5,25 @@ import { AiOutlineMenu } from "react-icons/ai";
 import MenuItem from "./MenuItem";
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const ref = useRef<HTMLDivElement>(null);
+  const innerRef = useRef<HTMLDivElement>(null);
+  const outerRef = useRef<HTMLDivElement>(null);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
 
   useEffect(()=>{
-    const handleClickOutsideRef=(event) =>{
-        if (ref.current && !ref.current.contains(event.target)) {
+    const handleClickOutsideRef=(event: MouseEvent) =>{
+        if (innerRef.current && !innerRef.current.contains(event.target as Node) && outerRef.current &&!outerRef.current.contains(event.target as Node)) {
             setIsOpen(false);
           }
     }
     document.addEventListener("mousedown", handleClickOutsideRef);
-  },[ref])
+  },[innerRef])
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3 rounded-full border-b-[1px] py-3 px-4 shadow-sm hover:shadow-md">
-        <div
+        <div ref={outerRef}
           onClick={toggleOpen}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-amber-300 transition cursor-pointer"
         >
@@ -33,7 +34,7 @@ const UserMenu = () => {
         </div>
       </div>
       {isOpen && (
-        <div ref={ref} className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+        <div ref={innerRef} className="absolute mt-2 rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="sign In" />
